4b7cbff3bcc1fb622e13889b6c16ce21abc34ee5
ZeroDawn0D <umang26112003@gmail.com>
2023-03-06 13:30:49 +0530

    added anim

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -27,32 +27,32 @@
       </router-link>
     </div>
     <div :class="`right-nav`">
-    	<div @click="updateNav(1)">
-      		<router-link :style = "`text-decoration: none; color: inherit`" to="/scitech">
+    	<div @click="updateNav(1)" :class="`scitech-button`">
+      		<router-link :style = "`text-decoration: none; color: inherit; `" to="/scitech">
       		SciTech
       		</router-link>
     	</div>
-    	<div  @click="updateNav(2)">
+    	<div  @click="updateNav(2)" :class="`cult-button`">
     		<router-link :style = "`text-decoration: none; color: inherit`" to="/cultural">
       		Culturals
       		</router-link>
     	</div>
-    	<div  @click="updateNav(3)">
+    	<div  @click="updateNav(3)" :class="`workshop-button`">
       		<router-link :style = "`text-decoration: none; color: inherit`" to="/workshop">
       		Workshop
       		</router-link>
     	</div>
-    	<div  @click="updateNav(4)">
+    	<div  @click="updateNav(4)" :class="`merch-button`">
       		<router-link :style = "`text-decoration: none; color: inherit`" to="/merch">
       		Merchandise
       		</router-link>
     	</div>
-    	<div  @click="updateNav(5)">
+    	<div  @click="updateNav(5)" :class="`spon-button`">
       		<router-link :style = "`text-decoration: none; color: inherit`" to="/sponsors">
       		Sponsors
       		</router-link>
     	</div>
-    	<div  @click="updateNav(6)">
+    	<div  @click="updateNav(6)" :class="`cont-button`">
     		<router-link :style = "`text-decoration: none; color: inherit`" to="/contact">
       		Contact
       		</router-link>
@@ -73,7 +73,7 @@
     display:flex;
     align-items: center;
     background-color: #3a0ca3;
-    color: #c89933;
+    color: #eeeeee;
     padding-left: 5vw;
     z-index: 2;
   }
@@ -90,13 +90,52 @@
   .right-nav div{
     padding: 8px;
     cursor:pointer;
-    transition: color 600ms, background-color 600ms;
-    border-radius: 10px;
+    transition: color 600ms, background-color 600ms, background-position 500ms;
+
+    background-size: 200% 200%;
+    background-position: 50% 0%;
+    border-radius: 18px;
   }
   .right-nav div:hover{
-    color: #3a0ca3;
-    background: #c89933;
+
+    background-position: 50% 60%;
   }
 
+  .scitech-button{
+    background-image: linear-gradient(to bottom,rgba(0,0,0,0) 50%, #f72585 100%);
+  }
+  .scitech-button:hover{
+    color: #f72585;
+  }
 
+  .cult-button{
+    background-image: linear-gradient(to bottom,rgba(0,0,0,0) 50%, #fff12d 100%);
+  }
+  .cult-button:hover{
+    color: #fff12d;
+  }
+  .workshop-button{
+    background-image: linear-gradient(to bottom,rgba(0,0,0,0) 50%, #3fd8a4 100%);
+  }
+  .workshop-button:hover{
+    color: #3fd8a4;
+  }  
+  .merch-button{
+    background-image: linear-gradient(to bottom,rgba(0,0,0,0) 50%, #7bff28 100%);
+  }
+  .merch-button:hover{
+    color: #7bff28;
+  }  
+  .spon-button{
+    background-image: linear-gradient(to bottom,rgba(0,0,0,0) 50%, #dd6e42 100%);
+  }
+  .spon-button:hover{
+    color: #dd6e42;
+  }  
+  .cont-button{
+    background-image: linear-gradient(to bottom,rgba(0,0,0,0) 50%, #3fd8a4 100%);
+  }
+  .cont-button:hover{
+    color: #3fd8a4;
+  }  
 </style>
--- src/components/Title.vue
+++ src/components/Title.vue
@@ -19,9 +19,53 @@ export default{
 </template>
 
 <style scoped>
+
+@keyframes appear100{
+	0%{
+		opacity: 0;
+		top: 90%
+	}
+	100%{
+		opacity: 1;
+		top: 70%;
+	}
+}
+
+@keyframes appear33{
+	0%{
+		left: 25vw;
+		opacity: 0;
+		top: 90%;
+	}
+	50%{
+		opacity: 0.33;
+		top: 70%;
+		left: 25vw;
+	}
+	100%{
+		left: 23vw;
+	}
+}
+
+@keyframes slideInFromBottom{
+	0%{
+		top: 90%;
+	}
+	100%{
+		top: 70%;
+	}
+}
+@keyframes blurAppear{
+	0%{
+		left: 25vw;
+	}
+	100%{
+		left: 23vw;
+	}
+}
 .title{
 	--theme-color: v-bind("color");
-	background-image: linear-gradient(to top, var(--theme-color) -10%, #3a0ca6 60%);
+	background-image: linear-gradient(to top, var(--theme-color) -10%, #3a0ca6 50%);
 	width: 25vw;
 	color: var(--theme-color);
 }
@@ -35,9 +79,13 @@ export default{
 	font-size: 5em;
 	letter-spacing: -5px;
 	font-weight: bolder;
+	animation: 0.4s ease-out 0s 1 appear100;
 }
 .transluscent{
 	left: 23vw;
+
+	animation: 0.8s ease-out 0s 1 appear33;
 	opacity:  0.33;
 }
+
 </style>
