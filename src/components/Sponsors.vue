c221fdb10e2ce69225360e4d810fb67aacd0585b
lo568los <99094516+lo568los@users.noreply.github.com>
2023-03-18 14:09:26 +0530

    stuff ig

--- src/components/Sponsors.vue
+++ src/components/Sponsors.vue
@@ -1,17 +1,32 @@
 <script>
 import Title from './Title.vue'
+import Card from './Card.vue'
 
 export default{
-	components:{Title}
+	components:{Title, Card}
 }
 </script>
 <template>
 	<div :class = "`title-flex`">
 		<Title text = "SPONSORS" color = "#dd6e42"/>
 		<div :class = "`content`">
-			stuff
+			<div :class = "'links'">
+				<Card link = "src/assets/Decoherence.jpg" 
+				size = "300px"/>
+				<Card link = "src/assets/BioBlitz.jpg" size = "300px"/>
+				<Card link = "src/assets/Chemenigma.jpg" size = "300px"/>
+				<Card link = "src/assets/Enumeration.jpg" size = "300px"/>
+				<Card link = "src/assets/Quadsparks.jpg" size = "300px"/>
+				<Card link = "src/assets/BAH.jpg" size = "300px"/>
+				<Card link = "src/assets/PIS.jpg" size = "300px"/>
+				<Card link = "src/assets/Exhibitions.jpg" size = "300px"/>
+			</div>
+		<div :class = "`content`">
+			No stuff
 		</div>
 	</div>
+
+</div>
 </template>
 
 <style>
